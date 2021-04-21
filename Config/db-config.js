a8058a90ebfacdb350e8d727648d122ca72bf7d3
Ibeandyson <ibeandyson123@gmail.com>
2021-04-21 02:34:17 +0100

    db setup

---
+++ Config/db-config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb+srv://db-artify9ja:db-artify9ja@artify9ja.oans9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      secretOrKey: "secret"
+};
--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-// modules importations
+// Importing needed modules
 const epress = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
 
 //init epress for useage
 const app = epress();
@@ -14,3 +17,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 //Use bodyparser to parse application/json
 app.use(bodyParser.json());
+
+//Passport middleware
+app.use(passport.initialize());
+
+//DB config
+const db = require('./Config/db-config').mongoURI;
+
+// connection to my cloud mongoDB
+mongoose
+    .connect(db, {useNewUrlParser: true})
+    .then(() => {
+        console.log('connected to mongoDB Atlas!');
+    })
+    .catch(error => {
+        console.log('not connected to mongoDB Atlas!');
+        console.error(error);
+    });
+
+module.exports = app;
