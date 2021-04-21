90589be48c3603140cc36d735ab5954a6fae816e
Ibeandyson <ibeandyson123@gmail.com>
2021-04-21 02:54:38 +0100

    added user model

---
+++ Models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        require: true,
+        unique: true,
+        trim: true,
+        minlength: 3
+    },
+    first_name: {
+        type: String,
+        require: true
+    },
+    last_name: {
+        type: String,
+        require: true
+    },
+    state: {
+        type: String,
+        require: true
+    },
+    address: {
+        type: String,
+        require: true
+    },
+    phone: {
+        type: String,
+        require: true,
+        minlength: 11
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = User = mongoose.model("users", UserSchema);
